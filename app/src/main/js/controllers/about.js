a01dc259f2c691cbad941a4df031b00ec79d2b79
Kwagala Derrick <derrick.kwagala@gmail.com>
2015-06-16 20:22:58 +0300

    initial app commit

---
+++ app/src/main/js/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pothunterWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pothunterWebApp
+ */
+angular.module('pothunterWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/src/main/js/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pothunterWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pothunterWebApp
+ */
+angular.module('pothunterWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
